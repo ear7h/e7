11ae49bcc7708ace77799de3318f694cc9a09bce
Julio <julio.grillo98@gmail.com>
2017-11-08 00:19:27 -0800

    expounded on remote test, now also testing dns services

--- server/remote_test.go
+++ server/remote_test.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"testing"
+	"time"
+	"github.com/ear7h/e7/e7c"
+	"net/http"
+	"github.com/miekg/dns"
+	"github.com/ear7h/e7"
+	"fmt"
 )
 
 func TestRemotely(t *testing.T) {
 	*SIBLING = "http://104.131.130.194"
 
-	main()
+	go main()
 
+	time.Sleep(1 * time.Second)
 
+	lst, err := e7c.Register("test-service")
+	if err != nil {
+		panic(err)
+	}
+
+	go http.Serve(lst, makePingHandler())
+
+	m := new(dns.Msg)
+	m.SetQuestion(e7.Hostname()+".ear7h.net.", dns.TypeA)
+
+	r, err := dns.Exchange(m, "104.131.130.194"+DNS_PORT)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(r)
 
 }
